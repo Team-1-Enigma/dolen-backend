9d4ab66ae8010ca88f3ba7f3f080636d807ba4ee
tsabit <qaultsabitm@gmail.com>
2024-05-17 22:25:28 +0700

    first commit

---
+++ src/main/java/com/enigma/dolen/DolenApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.dolen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/dolen/DolenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.dolen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
